937db024f4ba727ff5968312d8d597cd4c8e7bdc
ArthurSMR <arthursmr17@gmail.com>
2018-11-13 18:16:42 -0200

    salva em vetor completo

--- projeto.c
+++ projeto.c
@@ -3,14 +3,19 @@
 
 int main(int argc, char** argv)
 {
+    int total=0;
+    int numeros[1000];
+    FILE *f;
+    f = fopen(argv[1], "r");
     /*printf("%d\n", argc);
     for(int i=0;i<argc;i++){
         printf("argv[%d] = %s\n", i, argv[i]);
     } */
-//https://github.com/ArthurSMR/Projeto-Sistemas-Operacionais/invitations
-    FILE *f;
-    f = fopen(argv[1], "r");
+
     while(!feof(f)){
-        
+        fscanf(f, "%d", &numeros[total]);
+        total++;
     }
+    
+    return 0;
 }
